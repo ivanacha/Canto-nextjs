df79a180f448bd86772dabd7c0a5523bb9352616
Ivan C. Acha <ivanacha4@gmail.com>
2023-11-26 17:49:07 -0500

    Initial setup completed (#1)
    Good to go!

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,21 @@
 import React from "react"
 import { GetStaticProps } from "next"
-import Layout from "../components/Layout"
-import Post, { PostProps } from "../components/Post"
+import Layout from "@components/Layout"
+import Post, { PostProps } from "@components/Post"
+import prisma from "@lib/prisma"
 
 export const getStaticProps: GetStaticProps = async () => {
-  const feed = [
-    {
-      id: "1",
-      title: "Prisma is the perfect ORM for Next.js",
-      content: "[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!",
-      published: false,
+  const feed = await prisma.post.findMany({
+    // Filter only 'Post'records where published = true
+    where:  { published: true },
+    
+    // Include the 'author' and 'name' of the post.
+    include: {
       author: {
-        name: "Nikolas Burk",
-        email: "burk@prisma.io",
+        select: { name: true },
       },
     },
-  ]
+  })
   return { 
     props: { feed }, 
     revalidate: 10 
